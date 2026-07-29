09521fd79644c42c3be65b052aa7efe8a475c107
SpicyDogWings <spaceprotondev@proton.me>
2026-07-29 06:26:55 -0600

    Add 5 files, Update 6 files (11+ files changed)

--- default/.config/fish/config.fish
+++ default/.config/fish/config.fish
@@ -7,3 +7,6 @@ starship init fish | source
 set fish_greeting ""
 
 # fastfetch --logo "~/.config/fastfetch/logo.txt"
+
+# Pi
+fish_add_path "/home/spicydogwings/.vite-plus/js_runtime/node/24.18.0/bin"
