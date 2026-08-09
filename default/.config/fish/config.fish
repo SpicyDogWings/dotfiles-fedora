692a2ba8d81b8d392df3ffbdd3379e60d0e17b20
SpicyDogWings <spaceprotondev@proton.me>
2026-08-09 08:49:42 -0600

    Update 4 files: config.fish, config.toml, languages.toml, settings.json

--- default/.config/fish/config.fish
+++ default/.config/fish/config.fish
@@ -6,7 +6,23 @@ starship init fish | source
 
 set fish_greeting ""
 
+# zoxide (replaces cd)
+zoxide init fish | source
+alias cd=z
+
 # fastfetch --logo "~/.config/fastfetch/logo.txt"
 
 # Pi
 fish_add_path "/home/spicydogwings/.vite-plus/js_runtime/node/24.18.0/bin"
+
+# Added by jcode installer
+if not contains "/home/spicydogwings/.local/bin" $PATH
+    set -gx PATH "/home/spicydogwings/.local/bin" $PATH
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/spicydogwings/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
+end
+# pnpm end
