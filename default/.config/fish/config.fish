c29f8e63415198c610b5e08515db1d8ba8157433
spaceunandev <jocsan.fonseca@di.unanleon.edi.ni>
2026-08-20 13:08:08 -0600

    Update 2 files: alacritty.toml, config.fish

--- default/.config/fish/config.fish
+++ default/.config/fish/config.fish
@@ -16,8 +16,8 @@ alias cd=z
 # fastfetch --logo "~/.config/fastfetch/logo.txt"
 
 # pnpm
-set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if test -d "$PNPM_HOME" && not string match -q -- "$PNPM_HOME/bin" $PATH
+set -gx PNPM_HOME "/home/sheepcode/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
   set -gx PATH "$PNPM_HOME/bin" $PATH
 end
 # pnpm end
